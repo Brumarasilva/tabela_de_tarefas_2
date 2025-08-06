e28d4a04deb8d1038e3b169e335e67b392d0d504
Bruna Mara Silva <cristalbrumara@gmail.com>
2025-08-06 11:00:22 -0300

    adicionando pasta

---
+++ tabela_de_tarefas_3/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using tabela_de_tarefas_3.Models;
+
+namespace tabela_de_tarefas_3.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ tabela_de_tarefas_3/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tabela_de_tarefas_3.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
